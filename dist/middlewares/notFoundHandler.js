74b4c49b2fc74bc24571ada529dd424337c38940
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2026-01-07 16:54:34 +0200

    Remove dist folder from git tracking and update .gitignore

--- dist/middlewares/notFoundHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = notFoundHandler;
-function notFoundHandler(_req, res, _next) {
-    res.status(404).json({
-        error: "Not Found",
-    });
-}
--- dist/prisma.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const adapter_pg_1 = require("@prisma/adapter-pg");
-const client_1 = require("../prisma/generated/prisma/client");
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new adapter_pg_1.PrismaPg({ connectionString });
-exports.prisma = new client_1.PrismaClient({ adapter });
